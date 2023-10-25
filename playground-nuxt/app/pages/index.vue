2dfb00f9bc1587ed6a9507d66f0f60e5f63c5382
Hans <hans.doenitz@dasei.eu>
2023-10-25 12:02:24 +0200

    activate tailwind typography

--- playground-nuxt/app/pages/index.vue
+++ playground-nuxt/app/pages/index.vue
@@ -24,6 +24,12 @@ onMounted(async () => {
 <template>
   <div>
     <button @click="go">Go</button>
+      <div class="prose">
+        <h1>H1</h1>
+        <h2>H2</h2>
+        <h3>H3</h3>
+        <h4>H4</h4>
+      </div>
     <MainBanner />
 
   </div>
--- playground-nuxt/app/tailwind.config.ts
+++ playground-nuxt/app/tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'tailwindcss';
 import { tailwindConfig } from '@storefront-ui/vue/tailwind-config';
+import Typography from '@tailwindcss/typography';
 import sfTypography from '@storefront-ui/typography';
 
 export default <Config>{
   presets: [tailwindConfig],
   content: ['./**/*.vue', '../../node_modules/@storefront-ui/vue/**/*.{js,mjs}'],
-  plugins: [sfTypography],
-  css: ['~/assets/css/tailwind.css'],
+  plugins: [sfTypography, Typography],
+  css: ['~/assets/css/tailwind.css']
 };
