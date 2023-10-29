77d8f757d1ad60a4e33d6d507296c485960bfbf5
Hans <hans.doenitz@dasei.eu>
2023-10-29 11:18:56 +0100

    play around

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,10 +2,9 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
-
-  modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
-  ]
+  pinceau: {
+    followSymbolicLinks: false,
+    configFileName: 'tokens.config',
+    runtime: false,
+  }
 })
