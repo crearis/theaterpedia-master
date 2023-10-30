48523947551ec093d6d7e26f0d25fc7113b4c33a
Hans <hans.doenitz@dasei.eu>
2023-10-30 13:57:03 +0100

    moved homepage to own repo

--- playground-nuxt/homepage/content/about3.md
+++
@@ -1,5 +0,0 @@
-# About3
-
-# ich teste den About3 - File
-
-und jetzt sollte das wirklich funktionieren, die Prose-Tags werden aber falsch zugeordnet
--- playground-nuxt/homepage/server/queries/LoadUserQuery.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from '@apollo/client/core';
-import {partnerFragment} from './fragments';
-export default gql`
-  query LoadUser {
-    ${partnerFragment}
-  }
-`;
